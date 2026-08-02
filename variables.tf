16fcedad96bca5f8bd178cd3cc81e355a563cf45
raju-asapogu <asapoguraju@gmail.com>
2026-08-02 22:09:15 +0530

    terraform

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "ami_id" {
+  type = string
+  default = "ami-0220d79f3f480ecf5"
+  description = "this is the AMI used for creating EC2 instance"
+}
+
+variable "instance_type" {
+  type = string
+  description = "this is the instance type used for creating EC2 instance"
+  validation {
+    condition     = contains(["t3.micro", "t3.small", "t3.medium"], var.instance_type)
+    error_message = "Please Select either t3 micro or small or medium."
+  }
+}
+
+variable "sg_ids" {
+  type = list
+}
+
+variable "tags" {
+  type = map
+  default = {}
+    
+}
